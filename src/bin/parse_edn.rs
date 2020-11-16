233d18cb14588a104d40284d912e42a29b09dd89
Avery Crespi <averycrespi@gmail.com>
2020-11-16 09:43:58 -0800

    Fix entity parsing

--- src/bin/parse_edn.rs
+++ src/bin/parse_edn.rs
@@ -3,7 +3,6 @@ extern crate clap;
 use clap::{App, Arg};
 use rustbrew::edn::EdnStream;
 use rustbrew::entity::EntitySet;
-use serde_json;
 use std::fs;
 use std::str::FromStr;
 
@@ -12,10 +11,13 @@ fn main() {
         .arg(Arg::with_name("EDN_FILE").required(true))
         .get_matches();
     let edn_file = matches.value_of("EDN_FILE").unwrap();
-    let s = fs::read_to_string(edn_file).expect("failed to read EDN file");
+    let s = fs::read_to_string(edn_file).expect("failed to read EDN from file");
     let stream = EdnStream::from_str(&s).expect("failed to parse EDN");
-    let json = serde_json::to_string(&stream).expect("failed to serialize EDN");
-    println!("{}", json);
-    let entities: EntitySet = serde_json::from_str(&json).expect("failed to deserialize entities");
-    println!("{:?}", entities);
+    let json = serde_json::to_string_pretty(&stream).expect("failed to serialize EDN");
+    let json_file = format!("{}.json", edn_file);
+    fs::File::create(&json_file).expect("failed to create JSON file");
+    fs::write(json_file, &json).expect("failed to write JSON to file");
+    let entity_sets: Vec<EntitySet> =
+        serde_json::from_str(&json).expect("failed to deserialize entities");
+    println!("{:?}", entity_sets);
 }
